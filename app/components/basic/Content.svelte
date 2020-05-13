0e756116880d8bf092806a09288d03feba2e8d3f
AcrylicShrimp <led789zxpp@naver.com>
2020-05-13 19:40:10 +0900

    FEAT: Add a Content component.

---
+++ app/components/basic/Content.svelte
@@ -0,0 +1,19 @@
+<style lang="sass">
+  .content-container
+    padding-bottom: 120px
+
+  .content-container-inner
+    background: white
+    margin-left: auto
+    margin-right: auto
+    padding: 80px 30px 50px
+    max-width: 860px
+    min-height: 500px
+    box-shadow: 0px 3px 12px 0px rgba(0, 0, 0, 0.16)
+</style>
+
+<div class="content-container">
+  <div class="content-container-inner">
+    <slot />
+  </div>
+</div>
