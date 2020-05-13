b5514b29e05f19f32570e6ce6f2f18ee9eb4e031
AcrylicShrimp <led789zxpp@naver.com>
2020-05-13 19:40:55 +0900

    FEAT: Update the App component.

--- app/App.svelte
+++ app/App.svelte
@@ -1,5 +1,11 @@
 <script>
-  import Nav from "./components/nav/Nav";
+  import Content from "./components/basic/Content";
+  import Footer from "./components/basic/Footer";
+  import Nav from "./components/basic/Nav";
 </script>
 
 <Nav />
+<Content>
+  <p>wow</p>
+</Content>
+<Footer />
