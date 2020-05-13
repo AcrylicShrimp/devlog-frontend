ca6ff1828191589d9933be8a56a00dfc10d4624e
AcrylicShrimp <led789zxpp@naver.com>
2020-05-13 19:39:53 +0900

    FEAT: Add a Footer component.

---
+++ app/components/basic/Footer.svelte
@@ -0,0 +1,24 @@
+<style lang="sass">
+  .footer-container
+    min-height: 150px
+    background-color: #5D5D5D
+    box-shadow: 0px -6px 12px -6px rgba(0, 0, 0, 0.16)
+
+  .footer-container-inner
+    margin-left: auto
+    margin-right: auto
+    padding: 15px 12px
+    max-width: 860px
+
+  .footer-text
+    font-size: 16px
+    color: #dbdbdb
+</style>
+
+<footer class="footer-container">
+  <div class="footer-container-inner">
+    <span class="footer-text">&copy; {new Date().getFullYear() === 2020
+                    ? '2020'
+                    : `2020-${new Date().getFullYear()}`} AcrylicShrimp All rights reserved.</span>
+  </div>
+</footer>
