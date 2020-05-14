801d305398737950325df95e476642a64a955a62
AcrylicShrimp <led789zxpp@naver.com>
2020-05-15 08:42:19 +0900

    FEAT: Add a input svelte component.

---
+++ app/components/input/Input.svelte
@@ -0,0 +1,59 @@
+<style>
+	.error {
+		visibility: hidden;
+		margin: 0;
+		padding-left: 10px;
+		margin-top: 10px;
+		font-family: 'Roboto', sans-serif;
+		font-size: 13px;
+		color: #ff5a5a;
+	}
+
+	.error.show {
+		visibility: unset;
+	}
+
+	.input {
+		border: 0;
+		border-bottom: 2px solid #ffca64;
+		padding: 2px 4px;
+		width: 100%;
+		font-family: 'Nanum Gothic', 'Roboto', sans-serif;
+		font-size: 16px;
+		outline: none;
+	}
+</style>
+
+<script>
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+	let error = false;
+	export let hide = false;
+	export let disabled = false;
+	export let regex;
+	export let placeholder;
+	export let errmsg = '';
+
+	function onBlurInput(event) {
+		const value = event.target.value;
+
+		error = !regex.test(value);
+		dispatch('value', {
+			value: error ? undefined : value,
+		});
+	}
+</script>
+
+<div>
+	<input
+		class="input"
+		type="{hide ? 'password' : 'text'}"
+		disabled="{disabled}"
+		placeholder="{placeholder}"
+		on:focus="{() => (error = false)}"
+		on:blur="{onBlurInput}"
+	/>
+	<p class="error" class:show="{error}">{errmsg}</p>
+</div>
