c9a97140b3c74f05a635e4aad9e12b52d7ca468f
AcrylicShrimp <led789zxpp@naver.com>
2020-05-15 08:45:16 +0900

    FEAT: Add a SPA router.

--- app/App.svelte
+++ app/App.svelte
@@ -1,11 +1,29 @@
 <script>
-  import Content from "./components/basic/Content";
-  import Footer from "./components/basic/Footer";
-  import Nav from "./components/basic/Nav";
+	import Router from 'svelte-spa-router';
+	import { replace } from 'svelte-spa-router';
+
+	import Content from './components/basic/Content';
+	import Footer from './components/basic/Footer';
+	import Nav from './components/basic/Nav';
+
+	import List from './pages/List';
+	import Login from './pages/Login';
+	import NotFound from './pages/NotFound';
+	import Post from './pages/Post';
+
+	const routes = {
+		'/': List,
+		'/:slug': Post,
+		'/login': Login,
+		'*': NotFound,
+	};
+
+	if (!window.location.hash || window.location.hash == '#')
+		history.replaceState(undefined, undefined, '#/');
 </script>
 
 <Nav />
 <Content>
-  <p>wow</p>
+	<Router routes="{routes}" />
 </Content>
 <Footer />
