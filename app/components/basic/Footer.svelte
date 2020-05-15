b4cf7eb7aa4f67f443e333a1eeb629889525905e
AcrylicShrimp <led789zxpp@naver.com>
2020-05-15 20:36:20 +0900

    FEAT: Add administration menus in the footer.

--- app/components/basic/Footer.svelte
+++ app/components/basic/Footer.svelte
@@ -1,25 +1,109 @@
 <style>
 	.footer-container {
-		min-height: 150px;
 		background-color: #5d5d5d;
 		box-shadow: 0px -6px 12px -6px rgba(0, 0, 0, 0.16);
 	}
 	.footer-container-inner {
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		justify-content: start;
 		margin-left: auto;
 		margin-right: auto;
 		padding: 15px 12px;
 		max-width: 860px;
+		min-height: 150px;
 	}
 
 	.footer-text {
-		font-family: 'Roboto', sans-serif;
+		font-family: 'Raleway', sans-serif;
 		font-size: 16px;
 		color: #dbdbdb;
 	}
+
+	.footer-button {
+		display: block;
+		border: 1px solid #dbdbdb;
+		border-radius: 3px;
+		padding: 4px 8px;
+		width: 100%;
+		max-width: 150px;
+		background-color: transparent;
+		font-size: 14px;
+		text-align: center;
+		text-decoration: none;
+		cursor: pointer;
+		outline: none;
+	}
+
+	.footer-button + .footer-button {
+		margin-top: 5px;
+	}
+
+	.footer-text:last-child {
+		margin-top: auto;
+	}
 </style>
+
+<script>
+	import axios from 'axios';
+	import { link } from 'svelte-spa-router';
+
+	import { token } from '../../stores/token';
+
+	let loggingOut = false;
+	let apiToken;
+	let authenticated;
+
+	token.subscribe((token) => (authenticated = !!(apiToken = token)));
+
+	async function onClickLogout() {
+		if (loggingOut) return;
+
+		loggingOut = true;
+
+		try {
+			await axios.delete(
+				`https://api.blog.ashrimp.dev/admin/sessions/${apiToken}`
+			);
+
+			sessionStorage.setItem('api-token', null);
+			token.set(null);
+		} catch {
+		} finally {
+			loggingOut = false;
+		}
+	}
+</script>
 
 <footer class="footer-container">
 	<div class="footer-container-inner">
+		{#if authenticated}
+			<button
+				type="button"
+				class="footer-text footer-button"
+				on:click="{onClickLogout}"
+			>
+				Logout
+			</button>
+			<a
+				class="footer-text footer-button"
+				href="/editor"
+				target="_self"
+				use:link
+			>
+				Write a new post
+			</a>
+		{:else}
+			<a
+				class="footer-text footer-button"
+				href="/login"
+				target="_self"
+				use:link
+			>
+				Login
+			</a>
+		{/if}
 		<span class="footer-text">
 			&copy; {new Date().getFullYear() === 2020 ? '2020' : `2020-${new Date().getFullYear()}`}
 			AcrylicShrimp All rights reserved.
