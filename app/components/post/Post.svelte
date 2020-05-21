fc1e652f0fc5188b8cc9cbdf5b6e0e454836aad1
AcrylicShrimp <led789zxpp@naver.com>
2020-05-22 00:30:51 +0900

    REFACTOR: Split the post view component.

---
+++ app/components/post/Post.svelte
@@ -0,0 +1,64 @@
+<style>
+	.post-header-container {
+		display: flex;
+		flex-direction: row;
+		align-items: flex-start;
+		justify-content: space-between;
+	}
+
+	.post-category {
+		padding-left: 12px;
+		font-family: 'Nanum Myeongjo', serif;
+		font-size: 18px;
+		color: #727272;
+		visibility: hidden;
+	}
+
+	.post-category.exists {
+		visibility: unset;
+	}
+
+	.post-date {
+		font-family: 'Nanum Myeongjo', serif;
+		font-size: 18px;
+		color: #727272;
+	}
+
+	.post-title {
+		margin: 0;
+		margin-top: 32px;
+		font-family: 'Nanum Myeongjo', serif;
+		font-size: 34px;
+		font-size: bold;
+		color: #404040;
+	}
+
+	.post-content-container {
+		margin-top: 30px;
+	}
+</style>
+
+<script>
+	import 'font-awesome/css/font-awesome.css';
+
+	import './halflife.css';
+	import './Post.css';
+
+	import dayjs from 'dayjs';
+
+	export let category;
+	export let created = Date.now();
+	export let title;
+	export let content;
+</script>
+
+<div class="post-header-container">
+	<span class="post-category" class:exists="{category}">
+		{category ? category : ''}
+	</span>
+	<span class="post-date">{dayjs(created).format('YYYY/MM/DD HH:mm')}</span>
+</div>
+<h1 class="post-title">{title}</h1>
+<article class="post-content-container">
+	{@html content}
+</article>
--- app/pages/Post.svelte
+++ app/pages/Post.svelte
@@ -1,55 +1,10 @@
-<style>
-	.post-header-container {
-		display: flex;
-		flex-direction: row;
-		align-items: flex-start;
-		justify-content: space-between;
-	}
-
-	.post-category {
-		padding-left: 12px;
-		font-family: 'Nanum Myeongjo', serif;
-		font-size: 18px;
-		color: #727272;
-		visibility: hidden;
-	}
-
-	.post-category.exists {
-		visibility: unset;
-	}
-
-	.post-date {
-		font-family: 'Nanum Myeongjo', serif;
-		font-size: 18px;
-		color: #727272;
-	}
-
-	.post-title {
-		margin: 0;
-		margin-top: 32px;
-		font-family: 'Nanum Myeongjo', serif;
-		font-size: 34px;
-		font-size: bold;
-		color: #404040;
-	}
-
-	.post-content-container {
-		margin-top: 30px;
-	}
-</style>
-
 <script>
-	import 'font-awesome/css/font-awesome.css';
-
-	import './halflife.css';
-	import './Post.css';
-
 	import axios from 'axios';
-	import dayjs from 'dayjs';
 
 	import { token } from '../stores/token';
 
 	import Error from './Error';
+	import Post from '../components/post/Post';
 
 	let post = undefined;
 	let apiToken = null;
@@ -83,18 +38,12 @@
 </script>
 
 {#if post}
-	<div class="post-header-container">
-		<span class="post-category" class:exists="{post.category}">
-			{post.category ? post.category.name : ''}
-		</span>
-		<span class="post-date">
-			{dayjs(post.createdAt).format('YYYY/MM/DD HH:mm')}
-		</span>
-	</div>
-	<h1 class="post-title">{post.title}</h1>
-	<article class="post-content-container">
-		{@html post.htmlContent}
-	</article>
+	<Post
+		category="{post.category ? post.category.name : undefined}"
+		created="{post.createdAt}"
+		title="{post.title}"
+		content="{post.htmlContent}"
+	/>
 {:else if post === null}
 	<Error message="Post not found!" />
 {/if}
