225b9caafc3558a3f1be428012d5fd6d9da770d5
AcrylicShrimp <led789zxpp@naver.com>
2020-05-21 17:56:21 +0900

    FEAT: Add a new input label component.

---
+++ app/components/input/InputLabel.svelte
@@ -0,0 +1,23 @@
+<style>
+	.label {
+		margin: 0;
+		font-size: 16px;
+		font-family: inherit;
+		color: #1a1a1a;
+	}
+
+	.container {
+		margin-top: 15px;
+		padding-left: 20px;
+		font-family: inherit;
+	}
+</style>
+
+<script>
+	export let label;
+</script>
+
+<p class="label">{label}</p>
+<div class="container">
+	<slot />
+</div>
