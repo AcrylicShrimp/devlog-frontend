7f2074771617184c4121003d4e1daefd0b0d6533
AcrylicShrimp <led789zxpp@naver.com>
2020-06-06 22:49:03 +0900

    FEAT: Add pagenation.

--- app/pages/List.svelte
+++ app/pages/List.svelte
@@ -1,4 +1,40 @@
 <style>
+	.page-button-container {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: flex-start;
+		margin-bottom: 15px;
+	}
+
+	.page-button-container.top {
+		margin-bottom: 15px;
+	}
+
+	.page-button-container.bottom {
+		margin-top: 15px;
+	}
+
+	.page-button {
+		border: none;
+		border-radius: 4px;
+		width: 75px;
+		background-color: transparent;
+		font-size: 26px;
+		outline: none;
+		cursor: pointer;
+		box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.16);
+		transition: box-shadow 0.2s;
+	}
+
+	.page-button:hover {
+		box-shadow: 0px 0px 8px 0px rgba(0, 0, 0, 0.24);
+	}
+
+	.page-button.right {
+		margin-left: auto;
+	}
+
 	.post-link {
 		text-decoration: none;
 	}
@@ -63,6 +99,8 @@
 </style>
 
 <script>
+	import '../font-awesome/css/all.css';
+
 	import axios from 'axios';
 	import dayjs from 'dayjs';
 	import { link } from 'svelte-spa-router';
@@ -80,7 +118,12 @@
 		updatePostList();
 	});
 
-	async function updatePostList() {
+	async function updatePostList(before, after) {
+		let params = {};
+
+		if (before) params = Object.assign(params, { before });
+		if (after) params = Object.assign(params, { after });
+
 		try {
 			const result = await axios.get(
 				'https://api.blog.ashrimp.dev/posts',
@@ -88,6 +131,7 @@
 					headers: apiToken && {
 						'Api-Token': apiToken,
 					},
+					params,
 				}
 			);
 
@@ -107,6 +151,32 @@
 
 {#if posts}
 	{#if posts.posts.length}
+		<div class="page-button-container top">
+			{#if posts.hasAfter}
+				<button
+					class="page-button left"
+					on:click="{() => {
+						window.scrollTo(0, 0);
+						updatePostList(null, posts.posts[0].slug);
+						posts = undefined;
+					}}"
+				>
+					<i class="far fa-long-arrow-alt-left"></i>
+				</button>
+			{/if}
+			{#if posts.hasBefore}
+				<button
+					class="page-button right"
+					on:click="{() => {
+						window.scrollTo(0, 0);
+						updatePostList(posts.posts[posts.posts.length - 1].slug, null);
+						posts = undefined;
+					}}"
+				>
+					<i class="far fa-long-arrow-alt-right"></i>
+				</button>
+			{/if}
+		</div>
 		{#each posts.posts as post}
 			<a
 				class="post-link"
@@ -131,6 +201,32 @@
 				</article>
 			</a>
 		{/each}
+		<div class="page-button-container bottom">
+			{#if posts.hasAfter}
+				<button
+					class="page-button left"
+					on:click="{() => {
+						window.scrollTo(0, 0);
+						updatePostList(null, posts.posts[0].slug);
+						posts = undefined;
+					}}"
+				>
+					<i class="far fa-long-arrow-alt-left"></i>
+				</button>
+			{/if}
+			{#if posts.hasBefore}
+				<button
+					class="page-button right"
+					on:click="{() => {
+						window.scrollTo(0, 0);
+						updatePostList(posts.posts[posts.posts.length - 1].slug, null);
+						posts = undefined;
+					}}"
+				>
+					<i class="far fa-long-arrow-alt-right"></i>
+				</button>
+			{/if}
+		</div>
 	{:else}
 		<Error message="No post yet!" />
 	{/if}
